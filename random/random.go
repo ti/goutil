2ab6bfa605c04486ff9c25c04388e5e2b56750be
李南希 <leenanxi@gmail.com>
2018-08-23 15:28:49 +0800

    add radom

--- random/random.go
+++ random/random.go
@@ -22,7 +22,7 @@ func NewRandomByte(len int) []byte {
 	if _, err := io.ReadFull(rand.Reader, uuid); err != nil {
 		panic(err.Error()) // rand should never fail
 	}
-	if len > 10 {
+	if len > 8 {
 		uuid[6] = (uuid[6] & 0x0f) | 0x40 // Version 4
 		uuid[8] = (uuid[8] & 0x3f) | 0x80 // Variant is 10
 	}
